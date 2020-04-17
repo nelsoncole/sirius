479bd73da9ad292803c462da69409dab97d1cd37
nelsoncole <nelsoncole72@gmail.com>
2020-04-17 15:07:13 +0100

    commit++

--- user/nasm/version.h
+++
@@ -1,9 +0,0 @@
-#ifndef NASM_VERSION_H
-#define NASM_VERSION_H
-#define NASM_MAJOR_VER      0
-#define NASM_MINOR_VER      99
-#define NASM_SUBMINOR_VER   0
-#define NASM_PATCHLEVEL_VER 0
-#define NASM_VERSION_ID     0x00630000
-#define NASM_VER            "0.99.00"
-#endif /* NASM_VERSION_H */
