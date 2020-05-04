f537585df6dc85198d5edaf6afdd619557a0d1a8
nelsoncole <nelsoncole72@gmail.com>
2020-05-04 11:55:04 +0100

    commit++

--- user/nasm/test/imm64.asm
+++
@@ -1,7 +0,0 @@
-	bits 64
-	mov rax,11223344h
-	mov rax,dword 11223344h
-	mov eax,11223344h
-	mov [rax],dword 11223344h		; 32-bit operation
-	mov qword [rax],11223344h
-	mov qword [rax],dword 11223344h
--- user/nasm/version.nsh
+++
@@ -1,5 +0,0 @@
-!define VERSION "2.07"
-!define MAJOR_VER 7
-!define MINOR_VER 7
-!define SUBMINOR_VER 0
-!define PATCHLEVEL_VER 0
